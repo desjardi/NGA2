8ffda6902a734d10bbef0cd4e8442eef48059989
Olivier Desjardins <olivier.desjardins@cornell.edu>
2018-03-23 09:59:25 +0100

    Initiated work on config classes: these define the concept of a "config", i.e., grids+boundary conditions.

---
+++ src/config/ogconfig_class.f90
@@ -0,0 +1,10 @@
+!> One-grid config concept is defined here.
+!> The intention is to have a concept that mimics the original NGA,
+!> which will be evolved into mg_config (multi grid), and ag_config (adaptive grid)
+module ogconfig_class
+
+  implicit none
+  
+  
+  
+end module ogconfig_class
