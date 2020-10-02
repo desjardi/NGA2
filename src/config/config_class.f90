603e6ad2a00d1d011b33156f72ecd97d083482dc
Olivier Desjardins <olivier.desjardins@cornell.edu>
2020-10-02 17:47:14 -0400

    Starting work on config class.

--- src/config/config_class.f90
+++ src/config/config_class.f90
@@ -1,7 +1,7 @@
 !> Single-grid config concept is defined here.
-!> The intention is to have a concept that mimics the original NGA,
-!> which will be evolved into mgconfig (multi-grid), and agconfig (adaptive-grid)
+!> The intention is to have a concept that mimics the original NGA
 module config_class
+   use precision,   only: WP
    use bgrid_class, only: bgrid
    use pgrid_class, only: pgrid
    implicit none
@@ -12,17 +12,19 @@ module config_class
    
    !> Config object definition
    !> A "config" is essentially NGA's old config concept, but enhanced a bit.
-   !> Currently, it contains a pgrid, periodicity info, 
+   !> Currently, it contains a pgrid, periodicity info, more metrics, case name,
+   !> geometry info, and boundary conditions
    type, extends(pgrid) :: config
-      
-      ! Metrics
-      
-      ! Wall geometry
-      ! to do...
-      
+      ! Some more metrics
+      real(WP), dimension(:,:,:), allocatable :: vol           !< Local cell volume
+      real(WP), dimension(:,:,:), allocatable :: meshsize      !< Local effective cell size
+      real(WP) :: min_meshsize                                 !< Global minimum mesh size
+      ! Wall geometry - mask=0 is fluid, mask=1 is wall
+      integer,  dimension(:,:,:), allocatable :: mask          !< Masking info
       ! Boundary conditions
-      ! to do....
-      
+      integer :: xper,yper,zper                                !< Periodicity in x/y/z
+      integer :: nbound                                        !< Number of boundary conditions
+      type(bcond), dimension(:), pointer :: bc                 !< Storage array for boundary conditions
    contains
       procedure :: print=>config_print     !< Output configuration information to the screen
    end type config
