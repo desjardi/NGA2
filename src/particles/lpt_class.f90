22d125baaa81fdb4a4cf339e4c9c30b8726f2b17
Olivier Desjardins <olivier.desjardins@cornell.edu>
2021-03-06 11:02:45 -0500

    Particle restart i/o completed.

--- src/particles/lpt_class.f90
+++ src/particles/lpt_class.f90
@@ -18,6 +18,9 @@ module lpt_class
    real(WP), parameter :: coeff_up=1.3_WP      !< Particle array size increase factor
    real(WP), parameter :: coeff_dn=0.7_WP      !< Particle array size decrease factor
    
+   !> I/O chunk size to read at a time
+   integer, parameter :: part_chunk_size=1000  !< Read 1000 particles at a time before redistributing
+   
    
    !> Basic particle object definition
    type :: part
@@ -71,6 +74,8 @@ module lpt_class
       procedure :: resize                                 !< Resize particle array to given size
       procedure :: recycle                                !< Recycle particle array by removing flagged particles
       procedure :: sync                                   !< Synchronize particles across interprocessor boundaries
+      procedure :: read                                   !< Parallel read particles from file
+      procedure :: write                                  !< Parallel write particles to file
    end type lpt
    
    
@@ -97,7 +102,7 @@ contains
       self%cfg=>cfg
       
       ! Allocate variables
-      allocate(self%np_proc(self%cfg%nproc))
+      allocate(self%np_proc(1:self%cfg%nproc))
       
       ! Initialize MPI derived datatype for a particle
       call prepare_mpi_part()
@@ -145,10 +150,8 @@ contains
       integer :: rank_send,rank_recv,prank,ierr
       type(MPI_status) :: status
       type(part), dimension(:,:), allocatable :: buf_send
-      
-      ! Recycle first
+      ! Recycle first to minimize communication load
       call this%recycle()
-      
       ! Prepare information about who sends what to whom
       who_send=0
       do i=1,this%np_
@@ -157,19 +160,15 @@ contains
       end do
       ! Remove the diagonal since we won't self-send
       who_send(this%cfg%rank+1)=0
-      
       ! Prepare information about who receives what from whom
       do rank_recv=1,this%cfg%nproc
          call MPI_gather(who_send(rank_recv),1,MPI_INTEGER,who_recv,1,MPI_INTEGER,rank_recv-1,this%cfg%comm,ierr)
       end do
-      
       ! Prepare the buffers
       nb_send=maxval(who_send)
       nb_recv=sum(who_recv)
-      
       ! Allocate buffers to send particles
       allocate(buf_send(this%cfg%nproc,nb_send))
-      
       ! Find and pack the particles to be sent
       counter=0
       do i=1,this%np_
@@ -183,11 +182,9 @@ contains
             this%p(i)%flag=1
          end if
       end do
-      
       ! Everybody resizes
       np_old=this%np_
       call this%resize(this%np_+nb_recv)
-      
       ! Loop through the procs, pack particles in buf_send, send, unpack
       do rank_send=1,this%cfg%nproc
          if (this%cfg%rank+1.eq.rank_send) then
@@ -205,13 +202,10 @@ contains
             end if
          end if
       end do
-      
       ! Done, deallocate
       deallocate(buf_send)
-      
-      ! Recycle
+      ! Recycle to remove duplicate particles
       call this%recycle()
-      
    end subroutine sync
    
    
@@ -277,6 +271,138 @@ contains
       call MPI_ALLGATHER(this%np_,1,MPI_INTEGER,this%np_proc,1,MPI_INTEGER,this%cfg%comm,ierr)
       this%np=sum(this%np_proc)
    end subroutine recycle
+   
+   
+   !> Parallel write particles to file
+   subroutine write(this,filename)
+      use mpi_f08
+      use messager, only: die
+      implicit none
+      class(lpt), intent(inout) :: this
+      character(len=*), intent(in) :: filename
+      type(MPI_File) :: ifile
+      type(MPI_Status):: status
+      integer(kind=MPI_OFFSET_KIND) :: offset
+      integer :: i,ierr,iunit
+      
+      ! Root serial-writes the file header
+      if (this%cfg%amRoot) then
+         ! Open the file
+         open(newunit=iunit,file=trim(filename),form='unformatted',status='replace',access='stream',iostat=ierr)
+         if (ierr.ne.0) call die('[lpt write] Problem encountered while serial-opening data file: '//trim(filename))
+         ! Number of particles and particle object size
+         write(iunit) this%np,MPI_PART_SIZE
+         ! Done with the header
+         close(iunit)
+      end if
+      
+      ! The rest is done in parallel
+      call MPI_FILE_OPEN(this%cfg%comm,trim(filename),IOR(MPI_MODE_WRONLY,MPI_MODE_APPEND),info_mpiio,ifile,ierr)
+      if (ierr.ne.0) call die('[lpt write] Problem encountered while parallel-opening data file: '//trim(filename))
+      
+      ! Get current position
+      call MPI_FILE_GET_POSITION(ifile,offset,ierr)
+      
+      ! Compute the offset and write
+      do i=1,this%cfg%rank
+         offset=offset+int(this%np_proc(i),MPI_OFFSET_KIND)*int(MPI_PART_SIZE,MPI_OFFSET_KIND)
+      end do
+      if (this%np_.gt.0) call MPI_FILE_WRITE_AT(ifile,offset,this%p,this%np_,MPI_PART,status,ierr)
+      
+      ! Close the file
+      call MPI_FILE_CLOSE(ifile,ierr)
+      
+      ! Log/screen output
+      logging: block
+         use, intrinsic :: iso_fortran_env, only: output_unit
+         use param,    only: verbose
+         use messager, only: log
+         use string,   only: str_long
+         character(len=str_long) :: message
+         if (this%cfg%amRoot) then
+            write(message,'("Wrote ",i0," particles to file [",a,"] on partitioned grid [",a,"]")') this%np,trim(filename),trim(this%cfg%name)
+            if (verbose.gt.2) write(output_unit,'(a)') trim(message)
+            if (verbose.gt.1) call log(message)
+         end if
+      end block logging
+      
+   end subroutine write
+   
+   
+   !> Parallel read particles to file
+   subroutine read(this,filename)
+      use mpi_f08
+      use messager, only: die
+      implicit none
+      class(lpt), intent(inout) :: this
+      character(len=*), intent(in) :: filename
+      type(MPI_File) :: ifile
+      type(MPI_Status):: status
+      integer(kind=MPI_OFFSET_KIND) :: offset,header_offset
+      integer :: i,j,ierr,iunit,npadd,psize,nchunk
+      integer, dimension(:,:), allocatable :: ppp
+      
+      ! First open the file in parallel
+      call MPI_FILE_OPEN(self%cfg%comm,trim(filename),MPI_MODE_RDONLY,info_mpiio,ifile,ierr)
+      if (ierr.ne.0) call die('[lpt read] Problem encountered while reading data file: '//trim(filename))
+      
+      ! Read file header first
+      call MPI_FILE_READ_ALL(ifile,npadd,1,MPI_INTEGER,status,ierr)
+      call MPI_FILE_READ_ALL(ifile,psize,1,MPI_INTEGER,status,ierr)
+      
+      ! Remember current position
+      call MPI_FILE_GET_POSITION(ifile,header_offset,ierr)
+      
+      ! Check compatibility of particle type
+      if (psize.ne.MPI_PART_SIZE) call die('[lpt read] Particle type unreadable')
+      
+      ! Naively share reading task among all processors
+      nchunk=int(npadd/(this%cfg%nproc*part_chunk_size))+1
+      allocate(ppp(this%cfg%nproc,nchunk))
+      ppp=int(npadd/(this%cfg%nproc*nchunk))
+      count=0
+      out:do j=1,nchunk
+         do i=1,this%cfg%nproc
+            count=count+1
+            if (count.gt.mod(npadd,nproc*nchunk)) exit out
+            ppp(i,j)=ppp(i,j)+1
+         end do
+      end do out
+      
+      ! Read by chunk
+      do j=1,nchunk
+         ! Find offset
+         offset=header_offset+int(MPI_PART_SIZE,MPI_OFFSET_KIND)*int(sum(ppp(1:this%cfg%rank,:))+sum(ppp(this%cfg%rank+1,1:j-1)),MPI_OFFSET_KIND)
+         ! Resize particle array
+         call this%resize(this%np_+ppp(this%cfg%rank+1,j))
+         ! Read this file
+         call MPI_FILE_READ_AT(ifile,offset,this%p(this%np_+1:this%np_+ppp(this%cfg%rank+1,j)),ppp(this%cfg%rank+1,j),MPI_PART,status,ierr)
+         ! Most general case: relocate every droplet
+         do i=this%np_+1,this%np_+ppp(this%cfg%rank+1,j)
+            this%p(i)%ind=this%cfg%get_ijk_global(this%p(i)%pos,this%p(i)%ind)
+         end do
+         ! Exchange all that
+         call this%sync()
+      end do
+      
+      ! Close the file
+      call MPI_FILE_CLOSE(ifile,ierr)
+      
+      ! Log/screen output
+      logging: block
+         use, intrinsic :: iso_fortran_env, only: output_unit
+         use param,    only: verbose
+         use messager, only: log
+         use string,   only: str_long
+         character(len=str_long) :: message
+         if (this%cfg%amRoot) then
+            write(message,'("Read ",i0," particles from file [",a,"] on partitioned grid [",a,"]")') npadd,trim(filename),trim(this%cfg%name)
+            if (verbose.gt.2) write(output_unit,'(a)') trim(message)
+            if (verbose.gt.1) call log(message)
+         end if
+      end block logging
+      
+   end subroutine read
    
    
 end module lpt_class
