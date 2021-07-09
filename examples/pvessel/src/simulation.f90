18dd626058a02ec49d0be227cf9465f8566a52e0
Olivier Desjardins <olivier.desjardins@cornell.edu>
2021-07-09 15:38:10 -0400

    Faciliated the use of multiple (separate) inlets for pvessel case.

--- examples/pvessel/src/simulation.f90
+++ examples/pvessel/src/simulation.f90
@@ -41,8 +41,9 @@ module simulation
    
    !> Equation of state and case conditions
    real(WP) :: pressure,pressure_old,Vtotal
-   real(WP) :: MFR,Ain,rhoUin,fluid_mass,fluid_mass_old
-   real(WP) :: Tinit,Tinlet,Twall,Twallold,Tavg
+   real(WP), dimension(:), allocatable :: MFR,Ain,rhoUin,Tinlet
+   real(WP) :: fluid_mass,fluid_mass_old
+   real(WP) :: Tinit,Twall,Twallold,Tavg
    logical  :: wall_losses
    real(WP), parameter :: Wmlr=44.01e-3_WP  ! kg/mol
    real(WP), parameter :: Rcst=8.314_WP     ! J/(mol.K)
@@ -89,8 +90,8 @@ contains
    end function bag_at_loc
    
    
-   !> Function that localizes the top of the tube - y-face (v-vel)
-   function vtube(pg,i,j,k) result(isIn)
+   !> Functions that localize the top of the tube (y-face for v-vel and center for temp) - INLET 1
+   function vtube1(pg,i,j,k) result(isIn)
       use pgrid_class, only: pgrid
       implicit none
       class(pgrid), intent(in) :: pg
@@ -99,13 +100,8 @@ contains
       real(WP) :: r
       isIn=.false.
       if (abs(pg%xm(i)+dx-0.5_WP*lpipe).le.dx.and.pg%y(j)-0.5_WP*dy.lt.ypipe+rpipe.and.pg%y(j)+0.5_WP*dy.ge.ypipe+rpipe.and.abs(pg%zm(k)).lt.rpipe) isIn=.true.
-      if (abs(pg%xm(i)                ).le.dx.and.pg%y(j)-0.5_WP*dy.lt.ypipe+rpipe.and.pg%y(j)+0.5_WP*dy.ge.ypipe+rpipe.and.abs(pg%zm(k)).lt.rpipe) isIn=.true.
-      if (abs(pg%xm(i)-dx+0.5_WP*lpipe).le.dx.and.pg%y(j)-0.5_WP*dy.lt.ypipe+rpipe.and.pg%y(j)+0.5_WP*dy.ge.ypipe+rpipe.and.abs(pg%zm(k)).lt.rpipe) isIn=.true.
-   end function vtube
-   
-   
-   !> Function that localizes the top of the tube - scalar
-   function sctube(pg,i,j,k) result(isIn)
+   end function vtube1
+   function sctube1(pg,i,j,k) result(isIn)
       use pgrid_class, only: pgrid
       implicit none
       class(pgrid), intent(in) :: pg
@@ -114,9 +110,53 @@ contains
       real(WP) :: r
       isIn=.false.
       if (abs(pg%xm(i)+dx-0.5_WP*lpipe).le.dx.and.pg%ym(j).lt.ypipe+rpipe.and.pg%ym(j)+dy.ge.ypipe+rpipe.and.abs(pg%zm(k)).lt.rpipe) isIn=.true.
-      if (abs(pg%xm(i)                ).le.dx.and.pg%ym(j).lt.ypipe+rpipe.and.pg%ym(j)+dy.ge.ypipe+rpipe.and.abs(pg%zm(k)).lt.rpipe) isIn=.true.
+   end function sctube1
+   
+   
+   !> Functions that localize the top of the tube (y-face for v-vel and center for temp) - INLET 2
+   function vtube2(pg,i,j,k) result(isIn)
+      use pgrid_class, only: pgrid
+      implicit none
+      class(pgrid), intent(in) :: pg
+      integer, intent(in) :: i,j,k
+      logical :: isIn
+      real(WP) :: r
+      isIn=.false.
+      if (abs(pg%xm(i)).le.dx.and.pg%y(j)-0.5_WP*dy.lt.ypipe+rpipe.and.pg%y(j)+0.5_WP*dy.ge.ypipe+rpipe.and.abs(pg%zm(k)).lt.rpipe) isIn=.true.
+   end function vtube2
+   function sctube2(pg,i,j,k) result(isIn)
+      use pgrid_class, only: pgrid
+      implicit none
+      class(pgrid), intent(in) :: pg
+      integer, intent(in) :: i,j,k
+      logical :: isIn
+      real(WP) :: r
+      isIn=.false.
+      if (abs(pg%xm(i)).le.dx.and.pg%ym(j).lt.ypipe+rpipe.and.pg%ym(j)+dy.ge.ypipe+rpipe.and.abs(pg%zm(k)).lt.rpipe) isIn=.true.
+   end function sctube2
+   
+   
+   !> Functions that localize the top of the tube (y-face for v-vel and center for temp) - INLET 3
+   function vtube3(pg,i,j,k) result(isIn)
+      use pgrid_class, only: pgrid
+      implicit none
+      class(pgrid), intent(in) :: pg
+      integer, intent(in) :: i,j,k
+      logical :: isIn
+      real(WP) :: r
+      isIn=.false.
+      if (abs(pg%xm(i)-dx+0.5_WP*lpipe).le.dx.and.pg%y(j)-0.5_WP*dy.lt.ypipe+rpipe.and.pg%y(j)+0.5_WP*dy.ge.ypipe+rpipe.and.abs(pg%zm(k)).lt.rpipe) isIn=.true.
+   end function vtube3
+   function sctube3(pg,i,j,k) result(isIn)
+      use pgrid_class, only: pgrid
+      implicit none
+      class(pgrid), intent(in) :: pg
+      integer, intent(in) :: i,j,k
+      logical :: isIn
+      real(WP) :: r
+      isIn=.false.
       if (abs(pg%xm(i)-dx+0.5_WP*lpipe).le.dx.and.pg%ym(j).lt.ypipe+rpipe.and.pg%ym(j)+dy.ge.ypipe+rpipe.and.abs(pg%zm(k)).lt.rpipe) isIn=.true.
-   end function sctube
+   end function sctube3
    
    
    !> Function that localizes the left end of the tube
@@ -211,7 +251,17 @@ contains
       !   i=inflow%itr%map(1,n); j=inflow%itr%map(2,n); k=inflow%itr%map(3,n)
       !   sc%rho(i,j,k)=pressure*Wmlr/(Rcst*sc%SC(i,j,k))
       !end do
-      call sc%get_bcond('pipe inflow',inflow)
+      call sc%get_bcond('inlet 1',inflow)
+      do n=1,inflow%itr%no_
+         i=inflow%itr%map(1,n); j=inflow%itr%map(2,n); k=inflow%itr%map(3,n)
+         sc%rho(i,j,k)=pressure*Wmlr/(Rcst*sc%SC(i,j,k))
+      end do
+      call sc%get_bcond('inlet 2',inflow)
+      do n=1,inflow%itr%no_
+         i=inflow%itr%map(1,n); j=inflow%itr%map(2,n); k=inflow%itr%map(3,n)
+         sc%rho(i,j,k)=pressure*Wmlr/(Rcst*sc%SC(i,j,k))
+      end do
+      call sc%get_bcond('inlet 3',inflow)
       do n=1,inflow%itr%no_
          i=inflow%itr%map(1,n); j=inflow%itr%map(2,n); k=inflow%itr%map(3,n)
          sc%rho(i,j,k)=pressure*Wmlr/(Rcst*sc%SC(i,j,k))
@@ -280,14 +330,34 @@ contains
             end do
          end do
       end do
-   
-end subroutine get_cond
+      
+   end subroutine get_cond
    
    
    !> Initialization of problem solver
    subroutine simulation_init
-      use param, only: param_read
+      use param,    only: param_read
+      use messager, only: die
       implicit none
+      
+      
+      ! Process inlet conditions first
+      inlet_conditions: block
+         use param, only: param_getsize
+         integer :: ninlet_tmp,ninlet_mfr
+         ! Figure out how many inlets
+         ninlet_tmp=param_getsize('Inlet temperature')
+         ninlet_mfr=param_getsize('Inlet MFR (kg/s)')
+         if (ninlet_mfr.ne.ninlet_tmp) call die('[simulation_init] Inlet numbers are incompatible')
+         allocate(Tinlet(ninlet_tmp))
+         allocate(MFR(ninlet_mfr))
+         ! Read in temperature and MFR
+         call param_read('Inlet temperature',Tinlet)
+         call param_read('Inlet MFR (kg/s)',MFR)
+         ! Also allocate rhoUin and Ain
+         allocate(rhoUin(ninlet_mfr))
+         allocate(Ain(ninlet_mfr))
+      end block inlet_conditions
       
       
       ! Handle restart/saves here
@@ -366,7 +436,9 @@ end subroutine get_cond
          ! Define boundary conditions
          !call fs%add_bcond(name= 'left inflow',type=dirichlet,locator= left_of_tube    ,face='x',dir=+1,canCorrect=.false.)
          !call fs%add_bcond(name='right inflow',type=dirichlet,locator=right_of_tube_vel,face='x',dir=-1,canCorrect=.false.)
-         call fs%add_bcond(name= 'pipe inflow',type=dirichlet,locator=vtube,face='y',dir=-1,canCorrect=.false.)
+         call fs%add_bcond(name='inlet 1',type=dirichlet,locator=vtube1,face='y',dir=-1,canCorrect=.false.)
+         call fs%add_bcond(name='inlet 2',type=dirichlet,locator=vtube2,face='y',dir=-1,canCorrect=.false.)
+         call fs%add_bcond(name='inlet 3',type=dirichlet,locator=vtube3,face='y',dir=-1,canCorrect=.false.)
          ! Assign constant viscosity
          call param_read('Dynamic viscosity',visc); fs%visc=visc
          ! Assign acceleration of gravity
@@ -511,7 +583,9 @@ end subroutine get_cond
          ! Define boundary conditions
          !call sc%add_bcond(name= 'left inflow',type=dirichlet,locator= left_of_tube   )
          !call sc%add_bcond(name='right inflow',type=dirichlet,locator=right_of_tube_sc)
-         call sc%add_bcond(name='pipe inflow',type=dirichlet,locator=sctube)
+         call sc%add_bcond(name='inlet 1',type=dirichlet,locator=sctube1)
+         call sc%add_bcond(name='inlet 2',type=dirichlet,locator=sctube2)
+         call sc%add_bcond(name='inlet 3',type=dirichlet,locator=sctube3)
          if (wall_losses) call sc%add_bcond(name='wall',type=dirichlet,locator=wall_locator)
          ! Assign constant diffusivity
          call param_read('Dynamic diffusivity',diffusivity)
@@ -530,7 +604,6 @@ end subroutine get_cond
          integer :: n,i,j,k
          ! Read in the temperature and pressure
          call param_read('Initial temperature',Tinit)
-         call param_read('Inlet temperature',Tinlet)
          call param_read('Initial pressure',pressure)
          ! Handle restart
          if (restarted) then
@@ -563,11 +636,23 @@ end subroutine get_cond
          !   sc%SC(i,j,k)=Tinlet
          !   sc%rho(i,j,k)=pressure*Wmlr/(Rcst*Tinlet)
          !end do
-         call sc%get_bcond('pipe inflow',mybc)
+         call sc%get_bcond('inlet 1',mybc)
          do n=1,mybc%itr%no_
             i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
-            sc%SC(i,j,k)=Tinlet
-            sc%rho(i,j,k)=pressure*Wmlr/(Rcst*Tinlet)
+            sc%SC(i,j,k)=Tinlet(1)
+            sc%rho(i,j,k)=pressure*Wmlr/(Rcst*Tinlet(1))
+         end do
+         call sc%get_bcond('inlet 2',mybc)
+         do n=1,mybc%itr%no_
+            i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
+            sc%SC(i,j,k)=Tinlet(2)
+            sc%rho(i,j,k)=pressure*Wmlr/(Rcst*Tinlet(2))
+         end do
+         call sc%get_bcond('inlet 3',mybc)
+         do n=1,mybc%itr%no_
+            i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
+            sc%SC(i,j,k)=Tinlet(3)
+            sc%rho(i,j,k)=pressure*Wmlr/(Rcst*Tinlet(3))
          end do
          if (wall_losses) then
             call sc%get_bcond('wall',mybc)
@@ -603,10 +688,7 @@ end subroutine get_cond
             call df%pullvar(name='rhoW',var=fs%rhoW)
             call df%pullvar(name='P'   ,var=fs%P   )
          end if
-         ! Read in MFR
-         call param_read('Inlet MFR (kg/s)',MFR)
          ! Calculate inflow area
-         myAin=0.0_WP
          !call fs%get_bcond('left inflow',mybc)
          !do n=1,mybc%itr%n_
          !   i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
@@ -617,14 +699,28 @@ end subroutine get_cond
          !   i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
          !   myAin=myAin+fs%cfg%dy(j)*fs%cfg%dz(k)
          !end do
-         call fs%get_bcond('pipe inflow',mybc)
+         call fs%get_bcond('inlet 1',mybc)
+         myAin=0.0_WP
          do n=1,mybc%itr%n_
             i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
             myAin=myAin+fs%cfg%dx(i)*fs%cfg%dz(k)
          end do
-         call MPI_ALLREDUCE(myAin,Ain,1,MPI_REAL_WP,MPI_SUM,fs%cfg%comm,ierr)
+         call MPI_ALLREDUCE(myAin,Ain(1),1,MPI_REAL_WP,MPI_SUM,fs%cfg%comm,ierr)
+         myAin=0.0_WP
+         call fs%get_bcond('inlet 2',mybc)
+         do n=1,mybc%itr%n_
+            i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
+            myAin=myAin+fs%cfg%dx(i)*fs%cfg%dz(k)
+         end do
+         call MPI_ALLREDUCE(myAin,Ain(2),1,MPI_REAL_WP,MPI_SUM,fs%cfg%comm,ierr)
+         myAin=0.0_WP
+         call fs%get_bcond('inlet 3',mybc)
+         do n=1,mybc%itr%n_
+            i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
+            myAin=myAin+fs%cfg%dx(i)*fs%cfg%dz(k)
+         end do
+         call MPI_ALLREDUCE(myAin,Ain(3),1,MPI_REAL_WP,MPI_SUM,fs%cfg%comm,ierr)
          ! Form inflow momentum
-         rhoUin=MFR/Ain
          ! Apply Dirichlet at the tube
          !call fs%get_bcond('left inflow',mybc)
          !do n=1,mybc%itr%no_
@@ -636,10 +732,21 @@ end subroutine get_cond
          !   i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
          !   fs%rhoU(i,j,k)=+rhoUin
          !end do
-         call fs%get_bcond('pipe inflow',mybc)
+         rhoUin=MFR/Ain
+         call fs%get_bcond('inlet 1',mybc)
          do n=1,mybc%itr%no_
             i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
-            fs%rhoV(i,j,k)=+rhoUin
+            fs%rhoV(i,j,k)=+rhoUin(1)
+         end do
+         call fs%get_bcond('inlet 2',mybc)
+         do n=1,mybc%itr%no_
+            i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
+            fs%rhoV(i,j,k)=+rhoUin(2)
+         end do
+         call fs%get_bcond('inlet 3',mybc)
+         do n=1,mybc%itr%no_
+            i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
+            fs%rhoV(i,j,k)=+rhoUin(3)
          end do
          ! Set density from scalar
          fs%rho=0.5_WP*(sc%rho+sc%rhoold)
@@ -900,10 +1007,20 @@ end subroutine get_cond
                !   i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
                !   fs%rhoU(i,j,k)=+rhoUin
                !end do
-               call fs%get_bcond('pipe inflow',mybc)
+               call fs%get_bcond('inlet 1',mybc)
                do n=1,mybc%itr%no_
                   i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
-                  fs%rhoV(i,j,k)=+rhoUin
+                  fs%rhoV(i,j,k)=+rhoUin(1)
+               end do
+               call fs%get_bcond('inlet 2',mybc)
+               do n=1,mybc%itr%no_
+                  i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
+                  fs%rhoV(i,j,k)=+rhoUin(2)
+               end do
+               call fs%get_bcond('inlet 3',mybc)
+               do n=1,mybc%itr%no_
+                  i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
+                  fs%rhoV(i,j,k)=+rhoUin(3)
                end do
             end block mom_bcond
             
