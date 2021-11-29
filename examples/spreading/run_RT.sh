828ca16f1fefca9c046a081c0e722bfedac22b4e
Olivier Desjardins <olivier.desjardins@cornell.edu>
2021-11-29 15:40:12 -0500

    Added a first test of a film Rayleigh-Taylor case. Seems to do what's expected and form bags.

--- examples/spreading/run_RT.sh
+++ examples/spreading/run_RT.sh
@@ -8,8 +8,8 @@ OH=(1e-3 3e-3 1e-2 3e-2 1e-1 3e-1 1e+0 3e+0 1e+1 3e+1 1e+2)
 #   done
 #done
 
-# Just one test run with Oh=1e-1
+# Just one test run with Oh=1e-3
 for j in ${!CA[@]}; do
-   echo "Running a simulation with Oh=1e-1 and CA=${CA[$j]}"
-   mpiexec -n 4 ./nga.dp.gnu.opt.mpi.exe -i input  --Oh=1e-1 --CA=${CA[$j]} >> result.txt
+   echo "Running a simulation with Oh=1e-3 and CA=${CA[$j]}"
+   mpiexec -n 4 ./nga.dp.gnu.opt.mpi.exe -i input --Oh=1e-3 --CA=${CA[$j]} >> result.txt
 done
