8ea6ec1a934f3ed52fe14b2db18b1382dc58fa4f
Olivier Desjardins <olivier.desjardins@cornell.edu>
2021-12-05 17:32:02 -0500

    Added numerical solution to the full multiphase RT dispersion relation.

--- examples/multiphaseRT/plot_results.py
+++ examples/multiphaseRT/plot_results.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import optimize
 from matplotlib import pyplot as plt
 import re
 
@@ -22,8 +23,13 @@ with open('input') as fobj:
             Lx=float(line_data[1])
 
 # Create non-dimensional numbers
-At=(rho_l-rho_g)/(rho_l+rho_g)
-iGa=(2*np.pi)**3/(np.abs(gy)*Lx**3)*((mu_l+mu_g)/(rho_l+rho_g))**2
+Pl=rho_l/(rho_l+rho_g)
+Pg=rho_g/(rho_l+rho_g)
+At=Pl-Pg
+iGal=(2*np.pi)**3/(np.abs(gy)*Lx**3)*((     mu_l)/(     +rho_l))**2
+iGag=(2*np.pi)**3/(np.abs(gy)*Lx**3)*((mu_g     )/(rho_g      ))**2
+iGam=(2*np.pi)**3/(np.abs(gy)*Lx**3)*((mu_g-mu_l)/(rho_g+rho_l))**2
+iGap=(2*np.pi)**3/(np.abs(gy)*Lx**3)*((mu_g+mu_l)/(rho_g+rho_l))**2
 
 # Prepare plot
 plt.title('Multiphase Rayleigh-Taylor Instability')
@@ -33,10 +39,26 @@ plt.ylabel('Normalized growth rate')
 # Plot reference inviscid and viscous data
 ref_klc=np.linspace(0,1,num=1000)
 inviscid_ngr=np.sqrt(At*(ref_klc-ref_klc**3))
-viscous_ngr =np.sqrt(At*(ref_klc-ref_klc**3)+iGa*ref_klc)-np.sqrt(iGa*ref_klc)
+viscous_ngr =np.sqrt(At*(ref_klc-ref_klc**3)+iGap*ref_klc)-np.sqrt(iGap*ref_klc)
 plt.plot(ref_klc,inviscid_ngr,'-',lw=2,color='blue',label='Inviscid')
-plt.plot(ref_klc,viscous_ngr,'-',lw=2,color='green',label='Viscous')
+#plt.plot(ref_klc,viscous_ngr,'-',lw=2,color='green',label='Viscous approx')
 plt.ylim(bottom=0,top=1.2*max(inviscid_ngr))
+
+# Define dispersion relation for two-phase viscous RT
+def viscous_dispersion(x,klc,At,Pl,Pg,iGal,iGag,iGam):
+    if klc <= 0.0:
+        return (0.0)
+    else:
+        return ((At*klc*(1-klc**2)-x**2)*(Pl*np.sqrt(1+x/np.sqrt(iGag*klc))+Pg*np.sqrt(1+x/np.sqrt(iGal*klc))-1)
+        -4*Pl*Pg*x**2+4*np.sqrt(iGam*klc)*x*(Pl*np.sqrt(1+x/np.sqrt(iGag*klc))-Pg*np.sqrt(1+x/np.sqrt(iGal*klc))-At)
+        +4*iGam*klc*(np.sqrt(1+x/np.sqrt(iGag*klc))-1)*(np.sqrt(1+x/np.sqrt(iGal*klc))-1))
+
+# Plot exact viscous RT solution
+viscous2_ngr=0*ref_klc
+for ik, myklc in np.ndenumerate(ref_klc):
+    sol=optimize.root_scalar(viscous_dispersion,args=(myklc,At,Pl,Pg,iGal,iGag,iGam),bracket=[1e-8,2*inviscid_ngr[ik]],method='brentq')
+    viscous2_ngr[ik]=sol.root
+plt.plot(ref_klc,viscous2_ngr,'-',lw=2,color='blue',label='Viscous')
 
 # Plot results from result.txt file
 result=np.fromfile('result.txt',dtype=float,sep=' ')
