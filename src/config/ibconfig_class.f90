45f4d901a634c0ac9688b692804475bb13cc79ca
Olivier Desjardins <olivier.desjardins@cornell.edu>
2023-03-21 10:51:54 -0400

    Removed marching tets rescaling, but changed Gib interp to using dirichlet instead of neumann

--- src/config/ibconfig_class.f90
+++ src/config/ibconfig_class.f90
@@ -164,18 +164,18 @@ contains
                            do si=0,1
                               n=n+1
                               hex_vertex(:,n)=[this%x(i+si),this%y(j+sj),this%z(k+sk)]
-                              G(n)=-this%get_scalar(hex_vertex(:,n),i,j,k,this%Gib,'n')
+                              G(n)=-this%get_scalar(hex_vertex(:,n),i,j,k,this%Gib,'d')
                            end do
                         end do
                      end do
                      ! Rescale by min_meshsize and shift
-                     do n=1,8
-                        hex_vertex(:,n)=(hex_vertex(:,n)-[this%x(i),this%y(j),this%z(k)])/this%min_meshsize
-                        G(n)=G(n)/this%min_meshsize
-                     end do
+                     !do n=1,8
+                     !   hex_vertex(:,n)=(hex_vertex(:,n)-[this%x(i),this%y(j),this%z(k)])/this%min_meshsize
+                     !   G(n)=G(n)/this%min_meshsize
+                     !end do
                      ! Use marching tets to get volume fraction
                      call marching_tets(hex_vertex,G,vol,area,v_cent,a_cent)
-                     this%VF(i,j,k)=vol/this%vol(i,j,k)*(this%min_meshsize)**3
+                     this%VF(i,j,k)=vol/this%vol(i,j,k)!*(this%min_meshsize)**3
                   end do
                end do
             end do
