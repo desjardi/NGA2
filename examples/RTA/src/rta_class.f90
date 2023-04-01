8c5b3ecccd6b3b376d7612092e3926d9c869b34b
Olivier Desjardins <olivier.desjardins@cornell.edu>
2023-03-31 21:11:36 -0400

    RTA case seems to be working fine

---
+++ examples/RTA/src/rta_class.f90
@@ -0,0 +1,425 @@
+!> Definition for a Rayleigh-Taylor atomization class
+module rta_class
+   use precision,         only: WP
+   use config_class,      only: config
+   use iterator_class,    only: iterator
+   use ensight_class,     only: ensight
+   use hypre_str_class,   only: hypre_str
+   use ddadi_class,       only: ddadi
+   use vfs_class,         only: vfs
+   use tpns_class,        only: tpns
+   use timetracker_class, only: timetracker
+   use event_class,       only: event
+   use monitor_class,     only: monitor
+   implicit none
+   private
+   
+   public :: rta
+   
+   !> rta object
+   type :: rta
+      
+      !> Config
+      type(config) :: cfg
+      
+      !> Flow solver
+      type(vfs)         :: vf    !< Volume fraction solver
+      type(tpns)        :: fs    !< Two-phase flow solver
+      type(hypre_str)   :: ps    !< Structured Hypre linear solver for pressure
+      type(ddadi)       :: vs    !< DDADI solver for velocity
+      type(timetracker) :: time  !< Time info
+      
+      !> Ensight postprocessing
+      type(ensight)  :: ens_out  !< Ensight output for flow variables
+      type(event)    :: ens_evt  !< Event trigger for Ensight output
+      
+      !> Simulation monitor file
+      type(monitor) :: mfile    !< General simulation monitoring
+      type(monitor) :: cflfile  !< CFL monitoring
+      
+      !> Work arrays
+      real(WP), dimension(:,:,:), allocatable :: resU,resV,resW      !< Residuals
+      real(WP), dimension(:,:,:), allocatable :: Ui,Vi,Wi            !< Cell-centered velocities
+      
+      !> Iterator for VOF removal
+      type(iterator) :: vof_removal_layer  !< Edge of domain where we actively remove VOF
+      
+      !> Convective velocity
+      real(WP) :: Uconv
+      
+   contains
+      procedure :: init                            !< Initialize nozzle simulation
+      procedure :: step                            !< Advance nozzle simulation by one time step
+      procedure :: final                           !< Finalize nozzle simulation
+   end type rta
+
+   
+   !> Hardcode size of buffer layer for VOF removal
+   integer, parameter :: nlayer=5
+   
+contains
+   
+   
+   !> Initialization of rta simulation
+   subroutine init(this)
+      implicit none
+      class(rta), intent(inout) :: this
+      
+
+      ! Create the HIT mesh
+      create_config: block
+         use sgrid_class, only: cartesian,sgrid
+         use param,       only: param_read
+         use parallel,    only: group
+         real(WP), dimension(:), allocatable :: x,y
+         integer, dimension(3) :: partition
+         type(sgrid) :: grid
+         integer :: i,j,nx,ny
+         real(WP) :: Lx,dx
+         ! Read in grid size
+         call param_read('Number of cells',ny)
+         call param_read('Domain length',Lx)
+         ! Domain length is set to Lx and width to 5x5
+         dx=5.0_WP/real(ny,WP)
+         nx=int(Lx/dx)
+         ! Create simple rectilinear grid
+         allocate(x(nx+1),y(ny+1))
+         do i=1,nx+1
+            x(i)=real(i-1,WP)/real(nx,WP)*Lx
+         end do
+         do j=1,ny+1
+            y(j)=real(j-1,WP)/real(ny,WP)*5.0_WP !< Domain is of width 5 to get l_turb=1
+         end do
+         ! General serial grid object
+         grid=sgrid(coord=cartesian,no=3,x=x,y=y,z=y,xper=.false.,yper=.true.,zper=.true.,name='RTA')
+         ! Read in partition
+         call param_read('Partition',partition,short='p')
+         ! Create partitioned grid without walls
+         this%cfg=config(grp=group,decomp=partition,grid=grid)
+      end block create_config
+      
+
+      ! Initialize time tracker with 2 subiterations
+      initialize_timetracker: block
+         use param, only: param_read
+         this%time=timetracker(amRoot=this%cfg%amRoot)
+         call param_read('Max timestep size',this%time%dtmax)
+         call param_read('Max cfl number',this%time%cflmax)
+         this%time%dt=this%time%dtmax
+         this%time%itmax=2
+      end block initialize_timetracker
+      
+      
+      ! Allocate work arrays
+      allocate_work_arrays: block
+         allocate(this%resU(this%cfg%imino_:this%cfg%imaxo_,this%cfg%jmino_:this%cfg%jmaxo_,this%cfg%kmino_:this%cfg%kmaxo_))
+         allocate(this%resV(this%cfg%imino_:this%cfg%imaxo_,this%cfg%jmino_:this%cfg%jmaxo_,this%cfg%kmino_:this%cfg%kmaxo_))
+         allocate(this%resW(this%cfg%imino_:this%cfg%imaxo_,this%cfg%jmino_:this%cfg%jmaxo_,this%cfg%kmino_:this%cfg%kmaxo_))
+         allocate(this%Ui  (this%cfg%imino_:this%cfg%imaxo_,this%cfg%jmino_:this%cfg%jmaxo_,this%cfg%kmino_:this%cfg%kmaxo_))
+         allocate(this%Vi  (this%cfg%imino_:this%cfg%imaxo_,this%cfg%jmino_:this%cfg%jmaxo_,this%cfg%kmino_:this%cfg%kmaxo_))
+         allocate(this%Wi  (this%cfg%imino_:this%cfg%imaxo_,this%cfg%jmino_:this%cfg%jmaxo_,this%cfg%kmino_:this%cfg%kmaxo_))
+      end block allocate_work_arrays
+      
+      
+      ! Initialize our VOF solver and field
+      create_and_initialize_vof: block
+         use vfs_class, only: elvira
+         integer :: i,j,k
+         real(WP) :: xloc
+         ! Create a VOF solver with LVIRA
+         this%vf=vfs(cfg=this%cfg,reconstruction_method=elvira,name='VOF')
+         ! Initialize to flat interface in liquid needle
+         xloc=0.5_WP*this%cfg%xL
+         do k=this%vf%cfg%kmino_,this%vf%cfg%kmaxo_
+            do j=this%vf%cfg%jmino_,this%vf%cfg%jmaxo_
+               do i=this%vf%cfg%imino_,this%vf%cfg%imaxo_
+                  if (this%vf%cfg%xm(i).lt.xloc) then
+                     this%vf%VF(i,j,k)=1.0_WP
+                  else
+                     this%vf%VF(i,j,k)=0.0_WP
+                  end if
+                  this%vf%Lbary(:,i,j,k)=[this%vf%cfg%xm(i),this%vf%cfg%ym(j),this%vf%cfg%zm(k)]
+                  this%vf%Gbary(:,i,j,k)=[this%vf%cfg%xm(i),this%vf%cfg%ym(j),this%vf%cfg%zm(k)]
+               end do
+            end do
+         end do
+         ! Update the band
+         call this%vf%update_band()
+         ! Perform interface reconstruction from VOF field
+         call this%vf%build_interface()
+         ! Set interface planes at the boundaries
+         call this%vf%set_full_bcond()
+         ! Create discontinuous polygon mesh from IRL interface
+         call this%vf%polygonalize_interface()
+         ! Calculate distance from polygons
+         call this%vf%distance_from_polygon()
+         ! Calculate subcell phasic volumes
+         call this%vf%subcell_vol()
+         ! Calculate curvature
+         call this%vf%get_curvature()
+         ! Reset moments to guarantee compatibility with interface reconstruction
+         call this%vf%reset_volume_moments()
+      end block create_and_initialize_vof
+      
+      
+      ! Create an iterator for removing VOF at edges
+      create_iterator: block
+         this%vof_removal_layer=iterator(this%cfg,'VOF removal',vof_removal_layer_locator)
+      end block create_iterator
+
+      
+      ! Create a multiphase flow solver with bconds
+      create_flow_solver: block
+         use mathtools,       only: Pi
+         use param,           only: param_read
+         use hypre_str_class, only: pcg_pfmg
+         use tpns_class,      only: dirichlet,clipped_neumann,bcond
+         type(bcond), pointer :: mybc
+         integer :: n,i,j,k      
+         ! Create flow solver
+         this%fs=tpns(cfg=this%cfg,name='Two-phase NS')
+         ! Set fluid properties
+         this%fs%rho_l=1.0_WP
+         call param_read('Density ratio',this%fs%rho_g); this%fs%rho_g=this%fs%rho_l/this%fs%rho_g
+         this%fs%visc_l=(1.5_WP*this%cfg%min_meshsize/Pi)**(4.0_WP/3.0_WP)
+         call param_read('Viscosity ratio',this%fs%visc_g); this%fs%visc_g=this%fs%visc_l/this%fs%visc_g
+         call param_read('Weber number',this%fs%sigma); this%fs%sigma=1.0_WP/this%fs%sigma
+         call param_read('Bond number',this%fs%gravity(1)); this%fs%gravity(1)=this%fs%gravity(1)*this%fs%sigma
+         ! Read in convective velocity
+         call param_read('Convective velocity',this%Uconv)
+         ! Define inflow boundary condition on the left
+         call this%fs%add_bcond(name='inflow',type=dirichlet,face='x',dir=-1,canCorrect=.false.,locator=xm_locator)
+         ! Define outflow boundary condition on the right
+         call this%fs%add_bcond(name='outflow',type=clipped_neumann,face='x',dir=+1,canCorrect=.true.,locator=xp_locator)
+         ! Configure pressure solver
+         this%ps=hypre_str(cfg=this%cfg,name='Pressure',method=pcg_pfmg,nst=7)
+         this%ps%maxlevel=20
+         call param_read('Pressure iteration',this%ps%maxit)
+         call param_read('Pressure tolerance',this%ps%rcvg)
+         ! Configure implicit velocity solver
+         this%vs=ddadi(cfg=this%cfg,name='Velocity',nst=7)
+         ! Setup the solver
+         call this%fs%setup(pressure_solver=this%ps,implicit_solver=this%vs)
+         ! Zero initial field
+         this%fs%U=0.0_WP; this%fs%V=0.0_WP; this%fs%W=0.0_WP
+         ! Apply convective velocity
+         call this%fs%get_bcond('inflow',mybc)
+         do n=1,mybc%itr%no_
+            i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
+            this%fs%U(i,j,k)=this%Uconv
+         end do
+         ! Compute cell-centered velocity
+         call this%fs%interp_vel(this%Ui,this%Vi,this%Wi)
+         ! Compute divergence
+         call this%fs%get_div()
+      end block create_flow_solver
+      
+
+      ! Add Ensight output
+      create_ensight: block
+         use param, only: param_read
+         ! Create Ensight output from cfg
+         this%ens_out=ensight(cfg=this%cfg,name='rta')
+         ! Create event for Ensight output
+         this%ens_evt=event(time=this%time,name='Ensight output')
+         call param_read('Ensight output period',this%ens_evt%tper)
+         ! Add variables to output
+         call this%ens_out%add_vector('velocity',this%Ui,this%Vi,this%Wi)
+         call this%ens_out%add_scalar('VOF',this%vf%VF)
+         call this%ens_out%add_scalar('curvature',this%vf%curv)
+         call this%ens_out%add_scalar('pressure',this%fs%P)
+         ! Output to ensight
+         if (this%ens_evt%occurs()) call this%ens_out%write_data(this%time%t)
+      end block create_ensight
+      
+
+      ! Create a monitor file
+      create_monitor: block
+         ! Prepare some info about fields
+         call this%fs%get_cfl(this%time%dt,this%time%cfl)
+         call this%fs%get_max()
+         call this%vf%get_max()
+         ! Create simulation monitor
+         this%mfile=monitor(this%fs%cfg%amRoot,'simulation_atom')
+         call this%mfile%add_column(this%time%n,'Timestep number')
+         call this%mfile%add_column(this%time%t,'Time')
+         call this%mfile%add_column(this%time%dt,'Timestep size')
+         call this%mfile%add_column(this%time%cfl,'Maximum CFL')
+         call this%mfile%add_column(this%fs%Umax,'Umax')
+         call this%mfile%add_column(this%fs%Vmax,'Vmax')
+         call this%mfile%add_column(this%fs%Wmax,'Wmax')
+         call this%mfile%add_column(this%fs%Pmax,'Pmax')
+         call this%mfile%add_column(this%vf%VFmax,'VOF maximum')
+         call this%mfile%add_column(this%vf%VFmin,'VOF minimum')
+         call this%mfile%add_column(this%vf%VFint,'VOF integral')
+         call this%mfile%add_column(this%vf%SDint,'SD integral')
+         call this%mfile%add_column(this%fs%divmax,'Maximum divergence')
+         call this%mfile%add_column(this%fs%psolv%it,'Pressure iteration')
+         call this%mfile%add_column(this%fs%psolv%rerr,'Pressure error')
+         call this%mfile%write()
+         ! Create CFL monitor
+         this%cflfile=monitor(this%fs%cfg%amRoot,'cfl_atom')
+         call this%cflfile%add_column(this%time%n,'Timestep number')
+         call this%cflfile%add_column(this%time%t,'Time')
+         call this%cflfile%add_column(this%fs%CFLst,'STension CFL')
+         call this%cflfile%add_column(this%fs%CFLc_x,'Convective xCFL')
+         call this%cflfile%add_column(this%fs%CFLc_y,'Convective yCFL')
+         call this%cflfile%add_column(this%fs%CFLc_z,'Convective zCFL')
+         call this%cflfile%add_column(this%fs%CFLv_x,'Viscous xCFL')
+         call this%cflfile%add_column(this%fs%CFLv_y,'Viscous yCFL')
+         call this%cflfile%add_column(this%fs%CFLv_z,'Viscous zCFL')
+         call this%cflfile%write()
+      end block create_monitor
+      
+   end subroutine init
+   
+   
+   !> Take one time step
+   subroutine step(this)
+      implicit none
+      class(rta), intent(inout) :: this
+      
+      ! Increment time
+      call this%fs%get_cfl(this%time%dt,this%time%cfl)
+      call this%time%adjust_dt()
+      call this%time%increment()
+      
+      ! Remember old VOF
+      this%vf%VFold=this%vf%VF
+
+      ! Remember old velocity
+      this%fs%Uold=this%fs%U
+      this%fs%Vold=this%fs%V
+      this%fs%Wold=this%fs%W
+      
+      ! Prepare old staggered density (at n)
+      call this%fs%get_olddensity(vf=this%vf)
+         
+      ! VOF solver step
+      call this%vf%advance(dt=this%time%dt,U=this%fs%U,V=this%fs%V,W=this%fs%W)
+      
+      ! Prepare new staggered viscosity (at n+1)
+      call this%fs%get_viscosity(vf=this%vf)
+      
+      ! Perform sub-iterations
+      do while (this%time%it.le.this%time%itmax)
+         
+         ! Build mid-time velocity
+         this%fs%U=0.5_WP*(this%fs%U+this%fs%Uold)
+         this%fs%V=0.5_WP*(this%fs%V+this%fs%Vold)
+         this%fs%W=0.5_WP*(this%fs%W+this%fs%Wold)
+         
+         ! Preliminary mass and momentum transport step at the interface
+         call this%fs%prepare_advection_upwind(dt=this%time%dt)
+         
+         ! Explicit calculation of drho*u/dt from NS
+         call this%fs%get_dmomdt(this%resU,this%resV,this%resW)
+         
+         ! Add momentum source terms
+			call this%fs%addsrc_gravity(this%resU,this%resV,this%resW)
+         
+         ! Assemble explicit residual
+         this%resU=-2.0_WP*this%fs%rho_U*this%fs%U+(this%fs%rho_Uold+this%fs%rho_U)*this%fs%Uold+this%time%dt*this%resU
+         this%resV=-2.0_WP*this%fs%rho_V*this%fs%V+(this%fs%rho_Vold+this%fs%rho_V)*this%fs%Vold+this%time%dt*this%resV
+         this%resW=-2.0_WP*this%fs%rho_W*this%fs%W+(this%fs%rho_Wold+this%fs%rho_W)*this%fs%Wold+this%time%dt*this%resW   
+         
+         ! Form implicit residuals
+         call this%fs%solve_implicit(this%time%dt,this%resU,this%resV,this%resW)
+         
+         ! Apply these residuals
+         this%fs%U=2.0_WP*this%fs%U-this%fs%Uold+this%resU
+         this%fs%V=2.0_WP*this%fs%V-this%fs%Vold+this%resV
+         this%fs%W=2.0_WP*this%fs%W-this%fs%Wold+this%resW
+         
+         ! Solve Poisson equation
+         call this%fs%update_laplacian()
+         call this%fs%correct_mfr()
+         call this%fs%get_div()
+         call this%fs%add_surface_tension_jump(dt=this%time%dt,div=this%fs%div,vf=this%vf)
+         this%fs%psolv%rhs=-this%fs%cfg%vol*this%fs%div/this%time%dt
+         this%fs%psolv%sol=0.0_WP
+         call this%fs%psolv%solve()
+         call this%fs%shift_p(this%fs%psolv%sol)
+         
+         ! Correct velocity
+         call this%fs%get_pgrad(this%fs%psolv%sol,this%resU,this%resV,this%resW)
+         this%fs%P=this%fs%P+this%fs%psolv%sol
+         this%fs%U=this%fs%U-this%time%dt*this%resU/this%fs%rho_U
+         this%fs%V=this%fs%V-this%time%dt*this%resV/this%fs%rho_V
+         this%fs%W=this%fs%W-this%time%dt*this%resW/this%fs%rho_W
+         
+         ! Increment sub-iteration counter
+         this%time%it=this%time%it+1
+         
+      end do
+      
+      ! Recompute interpolated velocity and divergence
+      call this%fs%interp_vel(this%Ui,this%Vi,this%Wi)
+      call this%fs%get_div()
+      
+      ! Remove VOF at edge of domain
+      remove_vof: block
+         integer :: n
+         do n=1,this%vof_removal_layer%no_
+            this%vf%VF(this%vof_removal_layer%map(1,n),this%vof_removal_layer%map(2,n),this%vof_removal_layer%map(3,n))=0.0_WP
+         end do
+      end block remove_vof
+      
+      ! Output to ensight
+      if (this%ens_evt%occurs()) call this%ens_out%write_data(this%time%t)
+      
+      ! Perform and output monitoring
+      call this%fs%get_max()
+      call this%vf%get_max()
+      call this%mfile%write()
+      call this%cflfile%write()
+      
+      
+   end subroutine step
+   
+
+   !> Finalize nozzle simulation
+   subroutine final(this)
+      implicit none
+      class(rta), intent(inout) :: this
+      
+      ! Deallocate work arrays
+      deallocate(this%resU,this%resV,this%resW,this%Ui,this%Vi,this%Wi)
+      
+   end subroutine final
+   
+   
+   !> Function that localizes the x- boundary
+   function xm_locator(pg,i,j,k) result(isIn)
+      use pgrid_class, only: pgrid
+      class(pgrid), intent(in) :: pg
+      integer, intent(in) :: i,j,k
+      logical :: isIn
+      isIn=.false.
+      if (i.eq.pg%imin) isIn=.true.
+   end function xm_locator
+
+
+   !> Function that localizes the x+ boundary
+   function xp_locator(pg,i,j,k) result(isIn)
+      use pgrid_class, only: pgrid
+      class(pgrid), intent(in) :: pg
+      integer, intent(in) :: i,j,k
+      logical :: isIn
+      isIn=.false.
+      if (i.eq.pg%imax+1) isIn=.true.
+   end function xp_locator
+   
+   
+   !> Function that localizes region of VOF removal
+   function vof_removal_layer_locator(pg,i,j,k) result(isIn)
+      use pgrid_class, only: pgrid
+      class(pgrid), intent(in) :: pg
+      integer, intent(in) :: i,j,k
+      logical :: isIn
+      isIn=.false.
+      if (i.ge.pg%imax-nlayer) isIn=.true.
+   end function vof_removal_layer_locator
+   
+   
+end module rta_class
--- examples/RTA/src/simulation.f90
+++ examples/RTA/src/simulation.f90
@@ -1,15 +1,16 @@
 !> Various definitions and tools for running an NGA2 simulation
 module simulation
    use hit_class, only: hit
-   !use rta_class, only: rta
+   use rta_class, only: rta
    implicit none
    private
    
    !> HIT simulation
    type(hit) :: turb
-   
+   logical :: isInHITGrp
+
    !> Rayleigh-Taylor atomization simulation
-   !type(rta) :: atom
+   type(rta) :: atom
    
    public :: simulation_init,simulation_run,simulation_final
 
@@ -18,14 +19,40 @@ contains
    
    !> Initialization of our simulation
    subroutine simulation_init
-      use parallel, only: group
+      use mpi_f08, only: MPI_Group
       implicit none
+      type(MPI_Group) :: hit_group
       
       ! Initialize atomization simulation
-      !call atom%init()
-
+      call atom%init()
+      
+      ! Create an MPI group using leftmost processors only
+      create_hit_group: block
+         use parallel, only: group,comm
+         use mpi_f08,  only: MPI_Group_incl
+         integer, dimension(:), allocatable :: ranks
+         integer, dimension(3) :: coord
+         integer :: n,ngrp,ierr,ny,nz
+         ngrp=atom%cfg%npy*atom%cfg%npz
+         allocate(ranks(ngrp))
+         ngrp=0
+         do nz=1,atom%cfg%npz
+            do ny=1,atom%cfg%npy
+               ngrp=ngrp+1
+               coord=[0,ny-1,nz-1]
+               call MPI_CART_RANK(atom%cfg%comm,coord,ranks(ngrp),ierr)
+            end do
+         end do
+         call MPI_Group_incl(group,ngrp,ranks,hit_group,ierr)
+         if (atom%cfg%iproc.eq.1) then
+            isInHITGrp=.true.
+         else
+            isInHITGrp=.false.
+         end if
+      end block create_hit_group
+      
       ! Initialize HIT simulation
-      call turb%init(group=group)
+      if (isInHITGrp) call turb%init(group=hit_group)
       
    end subroutine simulation_init
    
@@ -35,14 +62,30 @@ contains
       implicit none
       
       ! Atomization drives overall time integration
-      !do while (.not.atom%time%done())
-      do while (.not.turb%time%done())
+      do while (.not.atom%time%done())
          
          ! Advance atomization simulation
-         !call atom%step()
+         call atom%step()
          
-         ! Advance HIT simulation
-         call turb%step(turb%time%dt)
+         ! Advance HIT simulation and transfer velocity info
+         if (isInHITGrp) then
+            ! Advance HIT
+            call turb%step(atom%time%dt)
+            ! Transfer turbulent velocity from hit to rta
+            apply_boundary_condition: block
+               use tpns_class, only: bcond
+               type(bcond), pointer :: mybc
+               integer :: n,i,j,k,ihit
+               call atom%fs%get_bcond('inflow',mybc)
+               do n=1,mybc%itr%no_
+                  i=mybc%itr%map(1,n); j=mybc%itr%map(2,n); k=mybc%itr%map(3,n)
+                  ihit=i-atom%fs%cfg%imin+turb%fs%cfg%imax+1
+                  atom%fs%U(i  ,j,k)=turb%fs%U(ihit  ,j,k)+atom%Uconv
+                  atom%fs%V(i-1,j,k)=turb%fs%V(ihit-1,j,k)
+                  atom%fs%W(i-1,j,k)=turb%fs%W(ihit-1,j,k)
+               end do
+            end block apply_boundary_condition
+         end if
          
       end do
       
@@ -54,10 +97,10 @@ contains
       implicit none
       
       ! Finalize atomization simulation
-      !call atom%final()
+      call atom%final()
       
       ! Finalize HIT simulation
-      call turb%final()
+      if (isInHITGrp) call turb%final()
       
    end subroutine simulation_final
    
