d83f43ef34953ffd1c6e0d70b288f03ff24de3fd
Olivier Desjardins <olivier.desjardins@cornell.edu>
2024-04-22 12:29:13 -0600

    Added calculation of surface moments transfer from fine to coarse mesh in film case (still need to test and debug)

--- examples/film/src/simulation.f90
+++ examples/film/src/simulation.f90
@@ -128,12 +128,203 @@ contains
       implicit none
       
       ! Integrate volume moments from fine to coarse mesh
-      
+      compute_volume_moments: block
+         integer :: fi,fj,fk
+         integer :: ci,cj,ck
+         ! Reset coarse volume moments
+         vf%VF=0.0_WP; vf%Lbary=0.0_WP; vf%Gbary=0.0_WP
+         ! Loop over fine mesh, find corresponding coarse mesh cell, integrate moments
+         do fk=dns%cfg%kmin_,dns%cfg%kmax_
+            ck=cfg%kmin_; do while (dns%cfg%zm(fk).ge.cfg%z(ck+1)); ck=ck+1; end do
+            do fj=dns%cfg%jmin_,dns%cfg%jmax_
+               cj=cfg%jmin_; do while (dns%cfg%ym(fj).ge.cfg%y(cj+1)); cj=cj+1; end do
+               do fi=dns%cfg%imin_,dns%cfg%imax_
+                  ci=cfg%imin_; do while (dns%cfg%xm(fi).ge.cfg%x(ci+1)); ci=ci+1; end do
+                  vf%VF(ci,cj,ck)=vf%VF(ci,cj,ck)+dns%vf%VF(fi,fj,fk)*dns%cfg%vol(fi,fj,fk)
+                  vf%Lbary(:,ci,cj,ck)=vf%Lbary(:,ci,cj,ck)+(       dns%vf%VF(fi,fj,fk))*dns%cfg%vol(fi,fj,fk)*dns%vf%Lbary(:,fi,fj,fk)
+                  vf%Gbary(:,ci,cj,ck)=vf%Gbary(:,ci,cj,ck)+(1.0_WP-dns%vf%VF(fi,fj,fk))*dns%cfg%vol(fi,fj,fk)*dns%vf%Gbary(:,fi,fj,fk)
+               end do
+            end do
+         end do
+         do ck=cfg%kmin_,cfg%kmax_
+            do cj=cfg%jmin_,cfg%jmax_
+               do ci=cfg%imin_,cfg%imax_
+                  vf%VF(ci,cj,ck)=vf%VF(ci,cj,ck)/cfg%vol(ci,cj,ck)
+                  vf%Lbary(:,ci,cj,ck)=vf%Lbary(:,ci,cj,ck)/cfg%vol(ci,cj,ck)
+                  vf%Gbary(:,ci,cj,ck)=vf%Gbary(:,ci,cj,ck)/cfg%vol(ci,cj,ck)
+               end do
+            end do
+         end do
+         call cfg%sync(vf%VF)
+         call vf%sync_and_clean_barycenters()
+      end block compute_volume_moments
       
       ! Integrate surface moments from fine to coarse mesh
-      
+      compute_surface_moments: block
+         use mpi_f08,  only: MPI_ALLREDUCE,MPI_SUM,MPI_IN_PLACE,MPI_INTEGER
+         use parallel, only: MPI_REAL_WP
+         use irl_fortran_interface
+         use vfs_class, only : VFlo,VFhi
+         integer :: fi,fj,fk,n,t,ierr
+         integer :: ci,cj,ck
+         type(DivPoly_type) :: divided_polygon
+         type(Tri_type) :: triangle
+         real(IRL_double), dimension(1:4) :: plane_data
+         real(IRL_double), dimension(:,:,:), allocatable :: tri,tmp
+         integer, dimension(:), allocatable :: ntri_proc
+         integer :: ntri,list_size,localizer_id,ii
+         real(WP), dimension(3) :: bary
+         integer, dimension(3) :: ind
+         type(TagAccListVM_VMAN_type) :: accumulated_moments_from_tri
+         type(ListVM_VMAN_type) :: moments_list_from_tri
+         type(VMAN_type) :: volume_moments_and_normal
+         ! Allocate IRL data
+         call new(divided_polygon)
+         call new(triangle)
+         call new(accumulated_moments_from_tri)
+         call new(moments_list_from_tri)
+         call new(volume_moments_and_normal)
+         ! Reset coarse surface moments
+         vf%SD=0.0_WP
+         do ck=cfg%kmino_,cfg%kmaxo_
+            do cj=cfg%jmino_,cfg%jmaxo_
+               do ci=cfg%imino_,cfg%imaxo_
+                  call clear(vf%triangle_moments_storage(ci,cj,ck))
+               end do
+            end do
+         end do
+         ! Count the number of surface triangles in DNS and communicate
+         allocate(ntri_proc(1:dns%cfg%nproc)); ntri_proc=0
+         do fk=dns%cfg%kmin_,dns%cfg%kmax_
+            do fj=dns%cfg%jmin_,dns%cfg%jmax_
+               do fi=dns%cfg%imin_,dns%cfg%imax_
+                  ! Skip if no interface
+                  if (dns%vf%VF(fi,fj,fk).lt.VFlo.or.dns%vf%VF(fi,fj,fk).gt.VFhi) cycle
+                  ! Construct triangulation of each interface plane
+                  do n=1,getNumberOfPlanes(dns%vf%liquid_gas_interface(fi,fj,fk))
+                     ! Skip planes outside of the cell
+                     if (getNumberOfVertices(dns%vf%interface_polygon(n,fi,fj,fk)).eq.0) cycle
+                     ! Get DividedPolygon from the plane
+                     call constructFromPolygon(divided_polygon,dns%vf%interface_polygon(n,fi,fj,fk))
+                     ! Increment triangle counter
+                     ntri_proc(dns%cfg%rank+1)=ntri_proc(dns%cfg%rank+1)+getNumberOfSimplicesInDecomposition(divided_polygon)
+                  end do
+               end do
+            end do
+         end do
+         call MPI_ALLREDUCE(MPI_IN_PLACE,ntri_proc,dns%cfg%nproc,MPI_INTEGER,MPI_SUM,dns%cfg%comm,ierr)
+         ntri=sum(ntri_proc)
+         ! Allocate global storage of our triangles
+         allocate(tri(1:3,1:3,ntri)); tri=0.0_WP
+         ! Create global triangulation of the DNS interface and communicate
+         ntri=0
+         do fk=dns%cfg%kmin_,dns%cfg%kmax_
+            do fj=dns%cfg%jmin_,dns%cfg%jmax_
+               do fi=dns%cfg%imin_,dns%cfg%imax_
+                  ! Skip if no interface
+                  if (dns%vf%VF(fi,fj,fk).lt.VFlo.or.dns%vf%VF(fi,fj,fk).gt.VFhi) cycle
+                  ! Construct triangulation of each interface plane
+                  do n=1,getNumberOfPlanes(dns%vf%liquid_gas_interface(fi,fj,fk))
+                     ! Skip planes outside of the cell
+                     if (getNumberOfVertices(dns%vf%interface_polygon(n,fi,fj,fk)).eq.0) cycle
+                     ! Get DividedPolygon from the plane
+                     call constructFromPolygon(divided_polygon,dns%vf%interface_polygon(n,fi,fj,fk))
+                     ! Check if point ordering correct, flip if not
+                     plane_data=getPlane(dns%vf%liquid_gas_interface(fi,fj,fk),n-1)
+                     if (abs(1.0_WP-dot_product(calculateNormal(divided_polygon),plane_data(1:3))).gt.1.0_WP) call reversePtOrdering(divided_polygon)
+                     ! Loop over triangles from DividedPolygon
+                     do t=1,getNumberOfSimplicesInDecomposition(divided_polygon)
+                        ! Increment triangle counter
+                        ntri=ntri+1
+                        ! Get the triangle vertices
+                        call getSimplexFromDecomposition(divided_polygon,t-1,triangle)
+                        ! Store at correct global location
+                        tri(1:3,1:3,sum(ntri_proc(1:dns%cfg%rank))+ntri)=getVertices(triangle)
+                     end do
+                  end do
+               end do
+            end do
+         end do
+         call MPI_ALLREDUCE(MPI_IN_PLACE,tri,9*dns%cfg%nproc,MPI_REAL_WP,MPI_SUM,dns%cfg%comm,ierr)
+         ! Finally, deal with periodicity - brute force copy
+         allocate(tmp(1:3,1:3,9*ntri)); tmp=0.0_WP
+         do n=1,ntri
+            ! Original triangle and its 8 copies
+            tmp(:,:,0*ntri+n)=tri(:,:,n) ! Original
+            tmp(:,:,1*ntri+n)=tri(:,:,n) ! x-
+            tmp(:,:,2*ntri+n)=tri(:,:,n) ! x+
+            tmp(:,:,3*ntri+n)=tri(:,:,n) ! z-
+            tmp(:,:,4*ntri+n)=tri(:,:,n) ! z+
+            tmp(:,:,5*ntri+n)=tri(:,:,n) ! x-/z-
+            tmp(:,:,6*ntri+n)=tri(:,:,n) ! x+/z-
+            tmp(:,:,7*ntri+n)=tri(:,:,n) ! x-/z+
+            tmp(:,:,8*ntri+n)=tri(:,:,n) ! x+/z+
+            ! Apply periodicity in x- (1, 5, 7)
+            tmp(1,:,1*ntri+n)=tmp(1,:,1*ntri+n)-dns%cfg%xL
+            tmp(1,:,5*ntri+n)=tmp(1,:,5*ntri+n)-dns%cfg%xL
+            tmp(1,:,7*ntri+n)=tmp(1,:,7*ntri+n)-dns%cfg%xL
+            ! Apply periodicity in x+ (2, 6, 8)
+            tmp(1,:,2*ntri+n)=tmp(1,:,2*ntri+n)+dns%cfg%xL
+            tmp(1,:,6*ntri+n)=tmp(1,:,6*ntri+n)+dns%cfg%xL
+            tmp(1,:,8*ntri+n)=tmp(1,:,8*ntri+n)+dns%cfg%xL
+            ! Apply periodicity in z- (3, 5, 6)
+            tmp(3,:,3*ntri+n)=tmp(3,:,3*ntri+n)-dns%cfg%zL
+            tmp(3,:,5*ntri+n)=tmp(3,:,5*ntri+n)-dns%cfg%zL
+            tmp(3,:,6*ntri+n)=tmp(3,:,6*ntri+n)-dns%cfg%zL
+            ! Apply periodicity in z+ (4, 7, 8)
+            tmp(3,:,4*ntri+n)=tmp(3,:,4*ntri+n)+dns%cfg%zL
+            tmp(3,:,7*ntri+n)=tmp(3,:,7*ntri+n)+dns%cfg%zL
+            tmp(3,:,8*ntri+n)=tmp(3,:,8*ntri+n)+dns%cfg%zL
+         end do
+         deallocate(tri); ntri=9*ntri; allocate(tri(1:3,1:3,ntri)); tri=tmp; deallocate(tmp)
+         ! Finally, populate the coarse surface data arrays
+         do ck=cfg%kmino_,cfg%kmaxo_
+            do cj=cfg%jmino_,cfg%jmaxo_
+               do ci=cfg%imino_,cfg%imaxo_
+                  ! Traverse all DNS triangles and store the ones local to our cell
+                  do n=1,ntri
+                     ! Get barycenter of triangle
+                     bary=(tri(:,1,n)+tri(:,2,n)+tri(:,3,n))/3.0_WP
+                     ! Test if it's in our cell
+                     if (bary(1).gt.cfg%x(ci).and.bary(1).lt.cfg%x(ci+1).and.&
+                     &   bary(2).gt.cfg%y(cj).and.bary(2).lt.cfg%y(cj+1).and.&
+                     &   bary(3).gt.cfg%z(ck).and.bary(3).lt.cfg%z(ck+1)) then
+                        ! Add the triangle moments
+                        call construct(triangle,tri(:,:,n))
+                        call calculateAndSetPlaneOfExistence(triangle)
+                        ! Cut it by the mesh
+                        call getMoments(triangle,vf%localizer_link(ci,cj,ck),accumulated_moments_from_tri)
+                        ! Loop through each cell and append to triangle_moments_storage in each cell
+                        list_size=getSize(accumulated_moments_from_tri)
+                        do ii=1,list_size
+                           localizer_id=getTagForIndex(accumulated_moments_from_tri,ii-1)
+                           ind=cfg%get_ijk_from_lexico(localizer_id)
+                           call getListAtIndex(accumulated_moments_from_tri,ii-1,moments_list_from_tri)
+                           call append(vf%triangle_moments_storage(ind(1),ind(2),ind(3)),moments_list_from_tri)
+                        end do
+                     end if
+                  end do
+               end do
+            end do
+         end do
+         ! Recompute surface density from previously calculated surface moments
+         vf%SD=0.0_WP
+         do ck=cfg%kmino_,cfg%kmaxo_
+            do cj=cfg%jmino_,cfg%jmaxo_
+               do ci=cfg%imino_,cfg%imaxo_
+                  do ii=0,getSize(vf%triangle_moments_storage(ci,cj,ck))-1
+                     call getMoments(vf%triangle_moments_storage(ci,cj,ck),ii,volume_moments_and_normal)
+                     vf%SD(ci,cj,ck)=vf%SD(ci,cj,ck)+getVolume(volume_moments_and_normal)
+                  end do
+                  vf%SD(ci,cj,ck)=vf%SD(ci,cj,ck)/cfg%vol(ci,cj,ck)
+               end do
+            end do
+         end do
+      end block compute_surface_moments
       
       ! Perform R2P reconstruction on coarse mesh
+      !----> Needs volume moments: VF, Lbary, and Gbary
+      !----> Needs surface moments: triangle_moments_storage and SD
       update_vf: block
          ! Update the band
          call vf%update_band()
