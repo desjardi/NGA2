b8e04e2a3a89d71a3efdcaedeb878b77aab65d25
Olivier Desjardins <olivier.desjardins@cornell.edu>
2024-05-14 09:31:49 -0400

    Slight changes to pdf plotting style

--- examples/hit_breakup/plot_pdf.py
+++ examples/hit_breakup/plot_pdf.py
@@ -1,10 +1,18 @@
 import numpy as np
 import matplotlib as plt
 import seaborn as sns
+import pandas as pd
 
 diam=[np.genfromtxt('diameter.elvira'),np.genfromtxt('diameter.plicnet')]
 
 sns.set_theme()
-#sns.displot(diam,bins=40,log_scale=True,stat="density",multiple="dodge",common_norm=False)
-sns.displot(diam,kind='kde',log_scale=True,common_norm=False,fill=True)
+# Histogram-style pdf
+#sns.displot(diam,bins=40,log_scale=True,stat='density',multiple='dodge',common_norm=False)
+
+# Log-log fitted pdf
+sns.displot(diam,kind='kde',log_scale=[True,True],common_norm=False)#,fill=True)
+
+# Log-lin fitted pdf
+#sns.displot(diam,kind='kde',log_scale=True,common_norm=False,fill=True)
+
 plt.pyplot.show()
