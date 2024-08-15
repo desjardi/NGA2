Major bug fix for the phase change velocity calculation.

examples/vaporizing_drop/plot_r_vs_t.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 import math
 
 # Reference values
-r0    = 0.005
+D0    = 0.01
+R0    = D0 / 2
 mflux = 5
 rhoL  = 1000
 muL   = 1.05e-3
-D0    = 2.0 * r0
-tref  = rhoL * D0**2 / muL
+# tref  = rhoL * D0**2 / muL
+tR  = R0 * rhoL / mflux
 
 # Load file
 data = np.loadtxt('./monitor/simulation', skiprows=2)
@@ -19,8 +20,7 @@ t = data[:, 1 ]
 r = data[:, 14]
 
 # Analytical solution
-r_ext = r0 - mflux * t
-# V_ext = math.pi * r_ext**2
+r_ext = R0 - mflux/rhoL * t
 
 # Use latex font
 plt.rcParams['text.usetex'] = True
@@ -30,15 +30,16 @@ plt.rcParams['font.family'] = 'serif'
 fig, ax = plt.subplots(1, 1, figsize=(4, 4))
 
 # Plot
-plt.plot(t/tref, r_ext/D0, ls='-'  , lw=2, color='k')
-plt.plot(t/tref, r    /D0, ls='--' , lw=2, color='b')
+plt.plot(t/tR, r_ext/R0, ls='-'  , lw=2, color='k')
+plt.plot(t/tR, r    /R0, ls='--' , lw=2, color='b')
 plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
 plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 plt.xlabel(r'$t/t_{ref}$', fontsize=12)
-plt.ylabel(r'$r/D_{0}$', fontsize=12)
+plt.ylabel(r'$r/R_{0}$', fontsize=12)
 plt.xticks(fontsize=10)
 plt.yticks(fontsize=10)
 plt.grid(which='major', axis='both', color='k', linestyle='--', linewidth=0.6, alpha=0.25)
 legends = ax.legend([r'$Analytical$', r'$Numerical$'], frameon=False, loc='lower left', fontsize=10)
 ax.add_artist(legends)
+plt.tight_layout()
 plt.savefig('./r_vs_t.pdf')