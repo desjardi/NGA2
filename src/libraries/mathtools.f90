432493d91abaec8a8187c8b1c97fca147ee633ed
jessecaps <jcaps@umich.edu>
2025-07-30 11:57:58 -0400

    Fixed but in inverse_matrix

--- src/libraries/mathtools.f90
+++ src/libraries/mathtools.f90
@@ -128,14 +128,13 @@ contains
       real(WP), dimension(size(A,1),size(A,2)) :: Ainv
       real(WP), dimension(size(A,1),size(A,2)) :: A_
       real(WP), dimension(size(A,2)) :: row_temp
-      integer :: i,j,n,pivot
-      real(WP) :: maxA
+      integer :: i,j,k,n,pivot
+      real(WP) :: maxA,factor
       ! Check if A is square
       if (size(A,1).ne.size(A,2)) call die('[inverse_matrix] Matrix is not square')
-      ! Store size
       n=size(A,1)
+      ! Copy A into A_ and initialize Ainv as identity matrix
       A_=A
-      ! Initialize Ainv to identity matrix
       Ainv=0.0_WP
       do i=1,n
          Ainv(i,i)=1.0_WP
@@ -151,7 +150,7 @@ contains
                pivot=j
             end if
          end do
-         ! Swap rows in A_ and Ainv if needed
+         ! Swap rows in A_ and Ainv
          if (pivot.ne.i) then
             row_temp=A_(i,:)
             A_(i,:)=A_(pivot,:)
@@ -160,27 +159,25 @@ contains
             Ainv(i,:)=Ainv(pivot,:)
             Ainv(pivot,:)=row_temp
          end if
-         ! Check for zero pivot
+         ! Check for singularity
          if (abs(A_(i,i)).lt.epsilon(A_(i,i))) call die('[inverse_matrix] Matrix is numerically singular')
          ! Normalize pivot row
-         A_(i,:)=A_(i,:)/A_(i,i)
-         Ainv(i,:)=Ainv(i, :)/A_(i,i)
-         ! Eliminate below
-         do j=i+1,n
-            A_(j, :)= A_(j,:)-A_(j,i)*A_(i,:)
-            Ainv(j,:)=Ainv(j,:)-A_(j,i)*Ainv(i,:)
-         end do
-      end do
-      ! Backward substitution
-      do i=n,1,-1
-         do j=i+1,n
-            Ainv(i,:)=Ainv(i,:)-A_(i,j)*Ainv(j,:)
+         factor=A_(i,i)
+         A_(i,:)=A_(i,:)/factor
+         Ainv(i,:)=Ainv(i,:)/factor
+         ! Eliminate other rows (not just below, all except i)
+         do j=1,n
+            if (j.ne.i) then
+               factor=A_(j,i)
+               A_(j,:)=A_(j,:)-factor*A_(i,:)
+               Ainv(j,:)=Ainv(j,:)-factor*Ainv(i,:)
+            end if
          end do
       end do
     end function inverse_matrix
 
-   
-   ! Returns normalized vector: w=v/|v|
+
+    ! Returns normalized vector: w=v/|v|
    pure function normalize(v) result(w)
       implicit none
       real(WP), dimension(3), intent(in) :: v
